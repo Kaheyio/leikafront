a1770e9522dd93094c52a3492d5f0d2ad540ed21
LDEV0422 <104555936+LDEV0422@users.noreply.github.com>
2022-07-18 06:45:46 +0200

    Test pdf generate (ok in dev)

--- src/app/components/logged/manage/documents/documents.component.ts
+++ src/app/components/logged/manage/documents/documents.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentsComponent implements OnInit {
 
+  @ViewChild('BISSheet') BISSheet!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // TODO: GET CREDIT ACCOUNT DATA
+  public generatePDF(): void {
+
+    html2canvas(this.BISSheet.nativeElement, { scale: 3 }).then((canvas) => {
+      const imageGeneratedFromTemplate = canvas.toDataURL('image/png');
+      const fileWidth = 200;
+      const generatedImageHeight = (canvas.height * fileWidth) / canvas.width;
+      let PDF = new jsPDF('p', 'mm', 'a4',);
+      PDF.addImage(imageGeneratedFromTemplate, 'PNG', 0, 5, fileWidth, generatedImageHeight,);
+      PDF.html(this.BISSheet.nativeElement.innerHTML)
+      PDF.save('angular-invoice-pdf-demo.pdf');
+    });
   }
 
 }
