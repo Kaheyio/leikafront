2c733b21d5a74182e2ed2b254c5e3abc6a831934
LDEV0422 <104555936+LDEV0422@users.noreply.github.com>
2022-07-18 02:46:41 +0200

    Test install chart js

--- src/app/components/logged/accounts/stats/stats.component.ts
+++ src/app/components/logged/accounts/stats/stats.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
+   labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  data = {
+    labels: this.labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+ config = {
+    type: 'line',
+    data: this.data,
+    options: {}
+  };
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart(){
+    
   }
 
 }
